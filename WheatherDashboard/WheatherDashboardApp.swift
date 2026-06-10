644d8f4b0e02212b791cfb64bb54d291c401d2bc
Pradeep-CG <pradeep.sagar.ios@gmail.com>
2026-06-10 19:58:30 +0530

    Initial Commit

---
+++ WheatherDashboard/WheatherDashboardApp.swift
@@ -0,0 +1,21 @@
+//
+//  WheatherDashboardApp.swift
+//  WheatherDashboard
+//
+//  Created by Pradeep Kumar Sagar on 10/06/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WheatherDashboardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
